5c76902d2e7bfc1ac369fb0f03982000bd0c05a2
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-12 14:54:29 -0800

    Added title and changed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   root 'users#new'
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: [:index, :new, :create, :edit, :update]
+  get 'main' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
